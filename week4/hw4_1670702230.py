7630529b0cb86716a670599cd230c50413792522
LUNEi3 <niranon49@gmail.com>
2025-02-06 18:27:19 +0700

    Complete hw4

--- week4/hw4_1670702230.py
+++ week4/hw4_1670702230.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+from tkinter import messagebox
 
 def root():
     root = tk.Tk()
     root.wm_geometry('1000x700+300+100')
-    root.title('Pariwat Niranon')
+    root.title('Homework of Week 4: Figure Shop by Pariwat Niranon')
     root.rowconfigure((0,2), weight=1)
     root.rowconfigure(1, weight=2)
     root.columnconfigure(0, weight=1)
@@ -35,11 +36,11 @@ def layout():
 
 
 def widgets():
-    btn1 = tk.Button(top, text="Click Me!", font=('sans', 16))
-    btn1.grid(row=0, column=0, ipadx=30, ipady=15, sticky='e')
+    btn1 = tk.Button(top, text="Click Me!", font=('sans', 16), image=icon2, compound='left', command=btnOnclick)
+    btn1.grid(row=0, column=0, ipadx=30, ipady=5, sticky='e')
 
-    btn2 = tk.Button(top, text="Exit Program", font=('sans', 16), command=exit)
-    btn2.grid(row=0, column=1, ipadx=30, ipady=15)
+    btn2 = tk.Button(top, text="Exit Program", font=('sans', 16), image=icon2, compound='left', command=exit)
+    btn2.grid(row=0, column=1, ipadx=30, ipady=5)
 
     fig1 = tk.Label(left, image=img1, bg='#EFE9D5')
     fig1.pack(pady=15)
@@ -50,26 +51,51 @@ def widgets():
     fig3 = tk.Label(left, image=img3, bg='#EFE9D5')
     fig3.pack(pady=15)
 
-    check1 = tk.Checkbutton(right, text='Skullpanda price = 380 THB', bg='#FFCDB2', font=('sans', 12))
-    check1.grid(row=0, column=1, sticky='w', padx=20)
+    check1 = tk.Checkbutton(right, text='Skullpanda price = 380 THB', bg='#FFCDB2', font=('sans', 12), variable=spy1, command=showTotal)
+    check1.grid(row=0, column=1, sticky='w')
 
-    check2 = tk.Checkbutton(right, text='Dimoo price = 450 THB', bg='#FFCDB2', font=('sans', 12))
-    check2.grid(row=1, column=1, sticky='w', padx=20)
+    check2 = tk.Checkbutton(right, text='Dimoo price = 450 THB', bg='#FFCDB2', font=('sans', 12), variable=spy2, command=showTotal)
+    check2.grid(row=1, column=1, sticky='w')
 
-    check3 = tk.Checkbutton(right, text='Crybaby price = 350 THB', bg='#FFCDB2', font=('sans', 12))
-    check3.grid(row=2, column=1, sticky='w', padx=20)
+    check3 = tk.Checkbutton(right, text='Crybaby price = 350 THB', bg='#FFCDB2', font=('sans', 12), variable=spy3, command=showTotal)
+    check3.grid(row=2, column=1, sticky='w')
 
     total = tk.Label(bottom, text="", font=('sans', 18, 'bold'), bg='#E5989B', fg="#0000FF")
     total.pack(expand=True)
     
     return total
 
+
+def btnOnclick():
+    messagebox.showinfo('About Shop', 'Welcome to Art Toy Shop by Popmart Official Store:\n1.Skullpanda\n2.Dimoo\n3.Crybaby')
+
+
+def showTotal():
+    sum = 0
+    if spy1.get() == 1:
+        sum += 380
+    if spy2.get() == 1:
+        sum += 450
+    if spy3.get() == 1:
+        sum += 350
+    total['text'] = f"Total Price = {sum} Bahts"
+    total['bg'] = '#EFE9D5'
+    
+
+
 master = root()
+
+# Spy
+spy1 = tk.IntVar()
+spy2 = tk.IntVar()
+spy3 = tk.IntVar()
 
 # Source
 img1 = tk.PhotoImage(file='image/fig1.png').subsample(4,4)
 img2 = tk.PhotoImage(file='image/fig2.png').subsample(4,4)
 img3 = tk.PhotoImage(file='image/fig3.png').subsample(3,3)
+icon1 = tk.PhotoImage(file='image/icon1.png')
+icon2 = tk.PhotoImage(file='image/icon2.png')
 
 top,left,right,bottom = layout()
 total = widgets()
