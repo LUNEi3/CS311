8757aee5eae24c73708d88d1eb309dac41b5bd01
LUNEi3 <niranon49@gmail.com>
2025-04-26 17:04:51 +0700

    Implement Add and Update features

--- final-practice/prac1.py
+++ final-practice/prac1.py
@@ -82,10 +82,11 @@ def welcomePage(result):
 
 
 def addClick():
+    global codeEntry, nameEntry, dayEntry, roomEntry
     bodyFrame.grid_forget()
     updateFrame.grid_forget()
     deleteFrame.grid_forget()
-    addFrame.rowconfigure((0,1,2,3), weight=1)
+    addFrame.rowconfigure((0,1,2,3,4,5), weight=1)
     addFrame.columnconfigure((0,1), weight=1)
     addFrame.grid(row=1, column=1, sticky="news")
     Label(addFrame, bg="#94B4C1", fg="black", text="Add Course", font="Garamond 26 bold").grid(row=0, column=0, columnspan=2)
@@ -95,13 +96,45 @@ def addClick():
     Label(addFrame, bg="#94B4C1", fg="black", text="Course Name: ").grid(row=2, column=0, sticky="e")
     nameEntry = Entry(addFrame, bg="white", fg="black")
     nameEntry.grid(row=2, column=1)
-    Button(addFrame, bg="white", fg="black", text="Add", width=10).grid(row=3, column=0, columnspan=2)
+    Label(addFrame, bg="#94B4C1", fg="black", text="Day: ").grid(row=3, column=0, sticky="e")
+    dayEntry = Entry(addFrame, bg="white", fg="black")
+    dayEntry.grid(row=3, column=1)
+    Label(addFrame, bg="#94B4C1", fg="black", text="Room: ").grid(row=4, column=0, sticky="e")
+    roomEntry = Entry(addFrame, bg="white", fg="black")
+    roomEntry.grid(row=4, column=1)
+    Button(addFrame, bg="white", fg="black", text="Add", command=addCourse,width=10).grid(row=5, column=0, columnspan=2)
 
 
 def updateClick():
+    global searchEntry, codeEntry, nameEntry, dayEntry, roomEntry
     bodyFrame.grid_forget()
     addFrame.grid_forget()
     deleteFrame.grid_forget()
+    updateFrame.rowconfigure((0,1,2,3,4,5,6), weight=1)
+    updateFrame.columnconfigure((0,1,2), weight=1)
+    updateFrame.grid(row=1, column=1, sticky="news")
+    Label(updateFrame, bg="#94B4C1", fg="black", text="Update Course", font="Garamond 26 bold").grid(row=0, column=0, columnspan=2)
+    Label(updateFrame, bg="#94B4C1", fg="black", text="Search Course Code: ").grid(row=1, column=0, sticky="e")
+    Button(updateFrame, bg="white", fg="black", text="Search", command=searchCourse,width=10).grid(row=1, column=2)
+    searchEntry = Entry(updateFrame, bg="white", fg="black")
+    searchEntry.grid(row=1, column=1)
+    
+    Label(updateFrame, bg="#94B4C1", fg="black", text="Course Code: ").grid(row=2, column=0, sticky="e")
+    codeEntry = Entry(updateFrame, bg="white", fg="black")
+    codeEntry.grid(row=2, column=1)
+
+    Label(updateFrame, bg="#94B4C1", fg="black", text="Course Name: ").grid(row=3, column=0, sticky="e")
+    nameEntry = Entry(updateFrame, bg="white", fg="black")
+    nameEntry.grid(row=3, column=1)
+    
+    Label(updateFrame, bg="#94B4C1", fg="black", text="Day: ").grid(row=4, column=0, sticky="e")
+    dayEntry = Entry(updateFrame, bg="white", fg="black")
+    dayEntry.grid(row=4, column=1)
+    
+    Label(updateFrame, bg="#94B4C1", fg="black", text="Room: ").grid(row=5, column=0, sticky="e")
+    roomEntry = Entry(updateFrame, bg="white", fg="black")
+    roomEntry.grid(row=5, column=1)
+    Button(updateFrame, bg="white", fg="black", text="Update", command=updateCourse,width=10).grid(row=6, column=0, columnspan=2)
 
 
 def deleteClick():
@@ -116,6 +149,71 @@ def logoutClick():
     pwdEntry.delete(0, END)
     loginPage()
     userEntry.focus_force()
+
+
+def searchCourse():
+    if searchEntry.get() == "":
+        messagebox.showerror("Admin", "Please Enter Course Code")
+        searchEntry.focus_force()
+    else:
+        sql = "SELECT * FROM course WHERE course_code=? COLLATE NOCASE"
+        cursor.execute(sql, [searchEntry.get()])
+        result = cursor.fetchone()
+        if result:
+            codeEntry.config(state="normal")
+            codeEntry.delete(0, END)
+            codeEntry.insert(0, result[1])
+            codeEntry.config(state="readonly")
+            nameEntry.delete(0, END)
+            nameEntry.insert(0, result[2])
+            dayEntry.delete(0, END)
+            dayEntry.insert(0, result[3])
+            roomEntry.delete(0, END)
+            roomEntry.insert(0, result[4])
+        else:
+            messagebox.showerror("Admin", "Invaid Course Code")
+            searchEntry.select_range(0, END)
+            searchEntry.focus_force()
+
+
+def addCourse():
+    if codeEntry.get() == "" or nameEntry.get() == "" or dayEntry.get() == "" or roomEntry.get() == "":
+        messagebox.showerror("Admin", "Please fullfill all of course data")
+        codeEntry.focus_force()
+    else:
+        sql = "SELECT * FROM course WHERE course_code=? AND course_name=?"
+        cursor.execute(sql, [codeEntry.get(), nameEntry.get()])
+        result = cursor.fetchone()
+        if result:
+            messagebox.showerror("Admin", "Course Code or Code Name is already exist")
+            codeEntry.select_range(0,END)
+            codeEntry.focus_force()
+        else:
+            sql = "INSERT INTO course VALUES (NULL,?,?,?,?)"
+            cursor.execute(sql, [codeEntry.get(), nameEntry.get(), dayEntry.get(), roomEntry.get()])
+            conn.commit()
+            messagebox.showinfo("Admin:","Add course successfully")
+            clearclick()
+
+
+def updateCourse():
+    if nameEntry.get() == "" or dayEntry.get() == "" or roomEntry.get() == "":
+        messagebox.showerror("Admin", "Please fulfill all of course data")
+        nameEntry.focus_force()
+    else:
+        sql = "UPDATE course SET course_name=?, day=?, room=? WHERE course_code=?"
+        cursor.execute(sql, [nameEntry.get(), dayEntry.get(), roomEntry.get(), codeEntry.get()])
+        conn.commit()
+        messagebox.showinfo("Admin", "Update course sucessfully")
+        clearclick()
+
+
+def clearclick() :
+    codeEntry.config(state='normal')
+    codeEntry.delete(0,END)
+    nameEntry.delete(0,END)
+    dayEntry.delete(0,END)
+    roomEntry.delete(0,END)
 
 
 connection()
